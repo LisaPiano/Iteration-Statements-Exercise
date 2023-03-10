34309ccfa513925a6e50835119cdd1486a38db67
Lisa Walters <lisacampiwalters@gmail.com>
2023-03-10 02:37:32 -0700

    added sentence

--- IterationStatements/Program.cs
+++ IterationStatements/Program.cs
@@ -4,6 +4,7 @@
     {
         //LukeWarm Section: Create methods below
         //Write a method that will print to the console all numbers 1000 through - 1000
+       
 
         //Write a method that will print to the console numbers 3 through 999 by 3 each time
 
@@ -27,7 +28,19 @@
         //Call the methods to test them in the Main method below
         static void Main(string[] args)
         {
-            
+            for (var i = 1000; i >= -1000; i--)
+            {
+                Console.WriteLine(i);
+                Console.WriteLine("hi");
+            }
+
+
+            for (var i = 3; i < 1000; i += 3)
+            { 
+                Console.WriteLine(i);
+            }
+
+
         }
     }
 }
